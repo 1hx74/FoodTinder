ff0fbacabb7ee67cc72a0673a696947703b817cf
maryin <maryin.kolya2017@gmail.com>
2026-09-08 10:36:24 +0500

    [feat] добавлен nginx и веб-интерфес (как документауия api), модифицирован compose.yml под новую конфигурацию

---
+++ web/server.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI, HTTPException, Response, Cookie
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+sessions = {}
+
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+
+@app.get("/")
+def main():
+    return FileResponse("main.html")
+
+
+@app.get("/authentication")
+def authentication():
+    return FileResponse("authentication.html")
+
+
+@app.post("/logout")
+def logout(
+    response: Response,
+    session: str | None = Cookie(default=None)
+):
+
+    if session:
+        sessions.pop(session, None)
+
+    response.delete_cookie("session")
+
+    return {
+        "message": "Logged out"
+    }
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000
+    )
